632aefc282fb38765d3fe6e4185e33119ec32f6c
FirasH94 <firas.hazim94@hotmail.com>
2020-06-28 15:56:53 +0000

    fixed the conenction to the database and mannaged to pull through the first table from MongoDB into HTML file.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+MONGO_URI = os.environ['MONGO_URI']
+app.config["MONGO_URI"] = MONGO_URI 
+app.config["MONGO_DBNAME"] = 'workout_tracker'
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello world'
+@app.route('/get_workouts')
+def get_workouts():
+    return render_template('workouts.html', workouts=mongo.db.workouts.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
