5a3794bacb843bd5d5e05b2ec43119d19e8bb4bf
FirasH94 <firas.hazim94@hotmail.com>
2020-07-18 16:01:54 +0000

    Adding delete workout functionality and creating categories page.

--- app.py
+++ app.py
@@ -29,6 +29,35 @@ def insert_workout():
     workouts.insert_one(request.form.to_dict())
     return redirect(url_for('get_workouts'))
 
+@app.route('/edit_workout/<workout_id>')
+def edit_workout(workout_id):
+    my_workout = mongo.db.workouts.find_one({"_id" : ObjectId(workout_id)})
+    all_categories = mongo.db.MuscleCategory.find()
+    return render_template('editworkout.html', workout=my_workout, categories=all_categories)
+
+@app.route('/update_workout/<workout_id>', methods=["POST"])
+def update_workout(workout_id):
+    workouts = mongo.db.workouts
+    workouts.update( {'_id': ObjectId(workout_id)},
+    {
+        'workout_name':request.form.get('workout_name'),
+        'category_name':request.form.get('category_name'),
+        'workout_notes': request.form.get('workout_notes'),
+        'sets': request.form.get('sets'),
+        'reps': request.form.get('reps'),
+        'pr_weight': request.form.get('pr_weight'),
+        'workout_date': request.form.get('workout_date')
+    })
+    return redirect(url_for('get_workouts'))
+
+@app.route('/delete_workout/<workout_id>')
+def delete_workout(workout_id):
+    mongo.db.workouts.remove({'_id': ObjectId(workout_id)})
+    return redirect(url_for('get_workouts'))
+
+@app.route('/get_categories')
+def get_categories():
+    return render_template('categories.html', categories=mongo.db.MuscleCategory.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
