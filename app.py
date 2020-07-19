dbe3bae38b911301ecf825227581b9a8a82ab2c7
FirasH94 <firas.hazim94@hotmail.com>
2020-07-19 12:11:05 +0000

    added delete, insert and edit category functionality.

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def update_workout(workout_id):
     workouts.update( {'_id': ObjectId(workout_id)},
     {
         'workout_name':request.form.get('workout_name'),
-        'category_name':request.form.get('category_name'),
+        'muscle_name':request.form.get('muscle_name'),
         'workout_notes': request.form.get('workout_notes'),
         'sets': request.form.get('sets'),
         'reps': request.form.get('reps'),
@@ -58,6 +58,34 @@ def delete_workout(workout_id):
 @app.route('/get_categories')
 def get_categories():
     return render_template('categories.html', categories=mongo.db.MuscleCategory.find())
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    return render_template('editcategory.html',
+                           category=mongo.db.MuscleCategory.find_one(
+                           {'_id': ObjectId(category_id)}))
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    mongo.db.MuscleCategory.update(
+        {'_id': ObjectId(category_id)},
+        {'muscle_name': request.form.get('muscle_name')})
+    return redirect(url_for('get_categories'))
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.MuscleCategory.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'muscle_name': request.form.get('muscle_name')}
+    mongo.db.MuscleCategory.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
